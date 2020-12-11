f33373e41206b5d4e70322407a9293a6edde1eed
Vince W <vincewallgren@me.com>
2020-12-11 04:40:58 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,12 +1,11 @@
+hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
 require 'pry'
 
 def hello(array)
   i = 0
   while i < array.length
+    binding.pry
     yield(array[i])
     i += 1
   end
 end
-
-
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
